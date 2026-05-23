18b80aff274ec8c699207dbbccaa750ea690dd47
baohan2909 <baohan2909@gmail.com>
2026-05-23 15:37:12 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  *   4. User bấm → skipWaiting + reload
  */
 
-const CACHE_VERSION = 'nonson-v9.40';
+const CACHE_VERSION = 'nonson-v9.41';
 const CACHE_NAME = `nonson-cache-${CACHE_VERSION}`;
 
 // Files cần precache cho offline (chỉ static assets)
