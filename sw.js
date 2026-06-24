7eafdd2eb923e5fdd73bfd2052dadff1041fb5bc
baohan2909 <baohan2909@gmail.com>
2026-06-24 18:04:15 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  *   4. User bấm → skipWaiting + reload
  */
 
-const CACHE_VERSION = 'nonson-v16.81';
+const CACHE_VERSION = 'nonson-v16.82';
 const CACHE_NAME = `nonson-cache-${CACHE_VERSION}`;
 
 // Files cần precache cho offline (chỉ static assets quan trọng)
